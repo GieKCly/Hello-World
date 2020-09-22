e8f022a478d5a0ee350f1bfeda0b36010c4c3363
Gie <70852519+GieKCly@users.noreply.github.com>
2020-09-22 10:18:08 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 /*
- * author: Gie Coulibaly
- * course: CS 141
- *     ID: 12750
+ *     author: Gie Coulibaly
+ * assignment: write a program that prints "Hello, world!"
+ *     course: CS 141
+ *         ID: 12750
  *
  */
 public class HelloWorld {
